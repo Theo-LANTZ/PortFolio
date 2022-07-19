2901e7dc0822be9a9e655ab040f3c7da84cdd637
Theo-LANTZ <theo.lantz57560@gmail.com>
2022-07-19 16:06:58 +0200

    init portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CompetenceComponent } from './competence/competence.component';
+import { HomeComponent } from './home/home.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { ProjetsComponent } from './projets/projets.component';
+import { MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    CompetenceComponent,
+    HomeComponent,
+    ProjetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
