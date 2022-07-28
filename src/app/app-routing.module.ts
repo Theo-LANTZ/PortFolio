df7e0b6921bfc3b7232770987bfee0a52dc242cd
Theo-LANTZ <theo.lantz57560@gmail.com>
2022-07-28 14:31:09 +0200

    new portfolio design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CompetenceComponent } from './competence/competence.component';
 import { HomeComponent } from './home/home.component';
-import { ProjetsComponent } from './projets/projets.component';
+import { MoreComponent } from './more/more.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'competence', component: CompetenceComponent},
-  { path: 'projets', component: ProjetsComponent},
-  { path: 'home', component: HomeComponent},
+  { path: 'more', component: MoreComponent},
+  { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
 import { MatTabsModule } from '@angular/material/tabs';
-import { CompetenceComponent } from './competence/competence.component';
 import { HomeComponent } from './home/home.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ProjetsComponent } from './projets/projets.component';
 import { MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { MatButtonModule} from '@angular/material/button';
+import { MoreComponent } from './more/more.component';
+import { MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
-    CompetenceComponent,
     HomeComponent,
     ProjetsComponent,
+    MoreComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +30,7 @@ import { MatButtonModule} from '@angular/material/button';
     MatCardModule,
     MatGridListModule,
     MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
